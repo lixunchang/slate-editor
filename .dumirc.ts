80d87ea0fabf07450cdceb5de214789bf0ab2b84
gexinpai <783263503@qq.com>
2024-10-29 00:52:21 +0800

    feat: 配置gitpage

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+    base: '/slate-editor',
+    publicPath: '/slate-editor',
+    exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
+    // 其他配置
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'slate-editor',

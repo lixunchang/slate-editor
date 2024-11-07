38981dd56cddcdbce9f93f3096da050a5221264f
gexinpai <783263503@qq.com>
2024-11-07 13:43:47 +0800

    feat: 静态文本路径

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
     base: '/slate-editor',
-    // publicPath: '/slate-editor',
+    publicPath: process.env.NODE_ENV === 'production' ? '/slate-editor/' : '/',
     exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
     // 其他配置
-  outputPath: 'docs-dist',
-  themeConfig: {
-    name: 'slate-editor',
-  },
-  lessLoader: {}
+    outputPath: 'docs-dist',
+    themeConfig: {
+      name: 'slate-editor',
+    },
+    lessLoader: {}
 });
